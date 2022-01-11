f6f2ed308ca56ceda8704aea0a73e2694c5df897
caner <canerserbest1997@gmail.com>
2022-01-11 04:10:45 +0300

    Console Programlama

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi giriniz.");
+            string surmane = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surmane);
+            
+            Console.ReadLine();
+        }
+    }
+}
